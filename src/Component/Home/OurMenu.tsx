c33420dd2020d551169b308a57af4d415275c347
Thuy Ha <116275455+Ha2k3@users.noreply.github.com>
2023-07-17 15:10:50 +0700

    finished Home

--- src/Component/Home/OurMenu.tsx
+++ src/Component/Home/OurMenu.tsx
@@ -1,5 +1,5 @@
 import { Col, Row, Button, Carousel, CarouselItem } from "react-bootstrap";
-
+import './OurMenu.css';
 const OurMenu = () => {
   return (
     <div className="ourmenu" style={{ backgroundColor: "#F8F8FF" }}>
@@ -9,15 +9,29 @@ const OurMenu = () => {
           <Col style={{ fontSize: "22px", fontWeight: "100" }}>
             Find your favorite food! We are very pleased to welcome you as guests in our restaurant.
           </Col>
-          <Button variant="outline-danger" style={{width:'40%'}}>See more</Button>
-        </Col>,
+          <Button variant="outline-danger" style={{ width: '40%' }}>See more</Button>
+        </Col>
         <Col>
           <Carousel>
             <CarouselItem>
-              <img src="https://www.eatthis.com/wp-content/uploads/sites/4/2019/06/deep-dish-pizza-chicago.jpg" alt="" style={{ objectFit: "cover", width: "100%", height: "300px" }} />
+              <div className="carousel-item-container">
+                <img src="https://www.eatthis.com/wp-content/uploads/sites/4/2019/06/deep-dish-pizza-chicago.jpg" alt="" className="carousel-item-image" />
+                <div className="carousel-item-info">
+                  <h3>Deep Dish Pizza</h3>
+                  <p>A delicious deep-dish pizza with a thick crust and savory toppings.</p>
+                  <p>Price : 10$</p>
+                </div>
+              </div>
             </CarouselItem>
             <CarouselItem>
-              <img src="https://www.tasteatlas.com/images/toplistarticles/08c818739e4b48ce96d319c16f4cc0ca.jpg" alt="" style={{ objectFit: "cover", width: "100%", height: "300px" }} />
+              <div className="carousel-item-container">
+                <img src="https://www.tasteatlas.com/images/toplistarticles/08c818739e4b48ce96d319c16f4cc0ca.jpg" alt="" className="carousel-item-image" />
+                <div className="carousel-item-info">
+                  <h3>Authentic Sushi</h3>
+                  <p>Enjoy fresh and authentic sushi with a variety of flavors and textures.</p>
+                  <p>Price : 10$</p>
+                </div>
+              </div>
             </CarouselItem>
           </Carousel>
         </Col>
