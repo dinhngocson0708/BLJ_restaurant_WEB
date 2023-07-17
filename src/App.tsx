91a1c4b0d4beff3df3caaf7bdc23ab09e2574a5b
Huong141709 <118873328+Huong141709@users.noreply.github.com>
2023-07-17 14:40:28 +0700

    create privacy

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 import Home from './Component/Home';
 import Contact from '../src/Component/Contact/Contact';
-import logo from './logo.svg';
 import './App.css';
+import PrivacyNotice from './Component/Privacy Notice';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/privacy' element={<PrivacyNotice />} />
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </BrowserRouter>
