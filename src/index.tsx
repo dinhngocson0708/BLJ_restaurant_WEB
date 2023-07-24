e8db8e25b8f40869c09b168b05cc88bdd3ad8552
Thuy Ha <116275455+Ha2k3@users.noreply.github.com>
2023-07-24 14:09:25 +0700

    update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PrismicProvider } from '@prismicio/react'
+import { client } from './prismic'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+      <PrismicProvider client={client}>
     <App />
+    </PrismicProvider>
   </React.StrictMode>
 );
 
