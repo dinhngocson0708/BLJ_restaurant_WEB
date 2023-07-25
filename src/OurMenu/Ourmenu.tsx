c64dc03712e980f9fb4fafe42a73b5cd7508409d
Quang200301 <117163705+Quang200301@users.noreply.github.com>
2023-07-25 16:10:21 +0700

    update

--- src/OurMenu/Ourmenu.tsx
+++ src/OurMenu/Ourmenu.tsx
@@ -3,93 +3,121 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { data } from './data';
 import Layout from '../Layout';
 import './ourmenu.css';
 import { Link } from 'react-router-dom';
+import { PrismicRichText, useAllPrismicDocumentsByType } from '@prismicio/react';
 const Ourmenu = () => {
-    interface MenuItem {
-        id: number;
-        img: string;
-        title: string;
-        desc: string;
-        price: number;
-      }
-    const riceData = data.filter((item) => item.category === "Cơm");
-    const chickenData = data.filter((item) => item.category === "Gà");
-    const familyData = data.filter((item) => item.category === "family");
-    const waterData = data.filter((item) => item.category === "nước");
+  interface Category {
+    type: string;
+    text: string;
+  }
+
+  const [documents] = useAllPrismicDocumentsByType('our_menu');
+  // Filter menu items by category
+  const chickenData = documents?.filter((item) =>
+    item.data.category.some((category: Category) => category.text === 'chicken')
+   
+  ); 
+  
+  const riceplate=documents?.filter((item)=>
+  item.data.category.some((category:Category)=>category.text === 'rice plate')
+  )
+  const family=documents?.filter((item)=>
+  item.data.category.some((category:Category)=>category.text === 'family')
+  )
+
+  const water=documents?.filter((item)=>
+  item.data.category.some((category:Category)=>category.text === 'beverage')
+  )
+
   return (
     <div>
-        
       <Layout>
         <h1 className='ourmenu__'>Our Menu</h1>
-        <img src='https://get.ringameal.com/App_Themes/Template04/img/bgHeaderPage.jpg'width='100%'/>
+        <img src='https://get.ringameal.com/App_Themes/Template04/img/bgHeaderPage.jpg' width='100%' />
         <Container>
           <Row>
             <h4 className='category-name'>CHICKEN</h4>
-            {chickenData.map((item:MenuItem) => (
+            {chickenData?.map((item) => (
               <Col md={4} key={item.id}>
                 <Card className='thumbnail' style={{ width: '26rem' }}>
-                <Link  to={`/detail/${item.id}`}> <Card.Img  variant="top" src={item.img} /></Link>
+                  <Link to={`/detail/${item.id}`}>
+                    <Card.Img variant='top' src={item.data.image.url} />
+                  </Link>
                   <Card.Body>
-                    <Card.Title>{item.title}</Card.Title>
-                    <Card.Text>
-                      Price: $ {item.price.toFixed(2)}
-                    </Card.Text>
-                    <Link className="btn btn-danger" to={`/detail/${item.id}`}>VIEW DETAIL</Link>
+                    <Card.Title className='name'  style={{ fontSize: '24px' }}>
+                      <PrismicRichText field={item.data.name}  />
+                    </Card.Title>
+                    <Card.Text className='price'> Price: $ <PrismicRichText field={item.data.price} /></Card.Text>
+                    <Link className='btn btn-danger' to={`/detail/${item.id}`}>
+                      VIEW DETAIL
+                    </Link>
                   </Card.Body>
                 </Card>
               </Col>
             ))}
             <h4 className='category-name'>RICE PLATE</h4>
-            {riceData.map((item:MenuItem) => (
+            {riceplate?.map((item) => (
               <Col md={4} key={item.id}>
                 <Card className='thumbnail' style={{ width: '26rem' }}>
-                <Link  to={`/detail/${item.id}`}> <Card.Img  variant="top" src={item.img} /></Link>
+                  <Link to={`/detail/${item.id}`}>
+                    <Card.Img variant='top' src={item.data.image.url} />
+                  </Link>
                   <Card.Body>
-                    <Card.Title>{item.title}</Card.Title>
-                    <Card.Text>
-                      Price: $ {item.price.toFixed(2)}
-                    </Card.Text>
-                    <Link className="btn btn-danger" to={`/detail/${item.id}`}>VIEW DETAIL</Link>
+                    <Card.Title className='name'  style={{ fontSize: '24px' }}>
+                      <PrismicRichText field={item.data.name}  />
+                    </Card.Title>
+                    <Card.Text className='price'> Price: $ <PrismicRichText field={item.data.price} /></Card.Text>
+                    <Link className='btn btn-danger' to={`/detail/${item.id}`}>
+                      VIEW DETAIL
+                    </Link>
                   </Card.Body>
                 </Card>
               </Col>
             ))}
-             <h4 className='category-name'>FAMILY MEALS</h4>
-            {familyData.map((item:MenuItem) => (
+            <h4 className='category-name'>FAMILY MEALS</h4>
+            {family?.map((item) => (
               <Col md={4} key={item.id}>
                 <Card className='thumbnail' style={{ width: '26rem' }}>
-                <Link  to={`/detail/${item.id}`}> <Card.Img  variant="top" src={item.img} /></Link>
+                  <Link to={`/detail/${item.id}`}>
+                    <Card.Img variant='top' src={item.data.image.url} />
+                  </Link>
                   <Card.Body>
-                    <Card.Title>{item.title}</Card.Title>
-                    <Card.Text>
-                      Price: $ {item.price.toFixed(2)}
-                    </Card.Text>
-                    <Link className="btn btn-danger" to={`/detail/${item.id}`}>VIEW DETAIL</Link>
+                    <Card.Title className='name'  style={{ fontSize: '24px' }}>
+                      <PrismicRichText field={item.data.name}  />
+                    </Card.Title>
+                    <Card.Text className='price'> Price: $ <PrismicRichText field={item.data.price} /></Card.Text>
+                    <Link className='btn btn-danger' to={`/detail/${item.id}`}>
+                      VIEW DETAIL
+                    </Link>
                   </Card.Body>
                 </Card>
               </Col>
             ))}
-             <h4 className='category-name'>Beverage</h4>
-            {waterData.map((item:MenuItem) => (
+             <h4 className='category-name'>BEVERAGE</h4>
+            {water?.map((item) => (
               <Col md={4} key={item.id}>
                 <Card className='thumbnail' style={{ width: '26rem' }}>
-                <Link  to={`/detail/${item.id}`}> <Card.Img  variant="top" src={item.img} /></Link>
+                  <Link to={`/detail/${item.id}`}>
+                    <Card.Img variant='top' src={item.data.image.url} />
+                  </Link>
                   <Card.Body>
-                    <Card.Title>{item.title}</Card.Title>
-                    <Card.Text>
-                      Price: $ {item.price.toFixed(2)}
-                    </Card.Text>
-                    <Link className="btn btn-danger" to={`/detail/${item.id}`}>VIEW DETAIL</Link>
+                    <Card.Title className='name'  style={{ fontSize: '24px' }}>
+                      <PrismicRichText field={item.data.name}  />
+                    </Card.Title>
+                    <Card.Text className='price'> Price: $ <PrismicRichText field={item.data.price} /></Card.Text>
+                    <Link className='btn btn-danger' to={`/detail/${item.id}`}>
+                      VIEW DETAIL
+                    </Link>
                   </Card.Body>
                 </Card>
               </Col>
             ))}
+          
           </Row>
         </Container>
-        </Layout>
+      </Layout>
     </div>
   );
 };
